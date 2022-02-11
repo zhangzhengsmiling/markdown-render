53e8170bda7ac7a8dfca37477998be6a74d648e6
zhangzhengsmiling <zhangzhengsmiling@163.com>
2022-02-11 13:58:40 +0800

    feat: render post

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
+const express = require('express');
 const hljs = require('highlight.js')
 const fs = require('fs');
 const path = require('path');
 const markdown = require('markdown-it')({
+  html: true,
   highlight: function (str, lang) {
     if (lang && hljs.getLanguage(lang)) {
       try {
@@ -9,19 +11,61 @@ const markdown = require('markdown-it')({
       } catch (__) {}
     }
 
-    return ''; // use external default escaping
+    return '';
   }
 });
 
-
-const data = fs.readFileSync(path.resolve(__dirname, './demo.md')).toString();
-
-const layout = (ch) => {
-  return (
-    "<html>" + "<head><link rel=\"stylesheet\" href=\"./demo.css\"></link></head><div class=\"post\">" + ch + "</div></html>"
-  )
+const markdownRender = (content) => {
+  return markdown.render(content)
+    .replaceAll(/<h1>(.*)<\/h1>/g, '<h1 id="$1">$1</h1>')
+    .replaceAll(/<h2>(.*)<\/h2>/g, '<h2 id="$1">$1</h2>')
+    .replaceAll(/<h3>(.*)<\/h3>/g, '<h3 id="$1">$1</h3>');
 }
 
-fs.writeFileSync(path.resolve(__dirname, 'demo.html'), layout(markdown.render(
-  data
-)))
+const move = (source, target, callback) => {
+  let sourceContent = fs.readFileSync(source);
+  if (typeof callback === 'function') sourceContent = callback(sourceContent);
+  fs.writeFileSync(target, sourceContent);
+}
+
+const posts = fs.readdirSync(path.resolve(__dirname, './source/posts'))
+posts.forEach(post => {
+  move(
+    path.resolve(__dirname, './source/posts', post),
+    path.resolve(__dirname, '../public/posts', post.replace('.md', '.html')),
+    stream => (
+      `<html>
+        <head>
+          <link rel="stylesheet" href="../css/code-theme.css">
+          <link rel="stylesheet" href="../css/post.css">
+        </head>
+        <body>
+          <div class="post">
+           ${markdownRender(stream.toString())}
+          </div>
+        </body>
+      </html>`
+    )
+  )
+})
+
+fs.cpSync(
+  path.resolve(__dirname, 'source/css'),
+  path.resolve(__dirname, '../public/css'),
+  {
+    recursive: true
+  }
+)
+
+move(
+  path.resolve(__dirname, '../node_modules/highlight.js/styles/atom-one-dark.css'),
+  path.resolve(__dirname, '../public/css', 'code-theme.css')
+)
+
+const app = express();
+
+app.use('/', express.static(path.resolve(__dirname, '../public')));
+
+app.listen(7777, () => {
+  console.log('server started on 7777')
+})
