40ed7d0a4ee91b1e8512dc457bc6b4b429fd82ae
zhangzhengsmiling <zhangzhengsmiling@163.com>
2022-02-11 17:35:44 +0800

    feat: latex支持，不完全，兼容性不好，考虑用typera编辑后贴图

--- src/app.js
+++ src/app.js
@@ -1,43 +1,47 @@
-const express = require('express');
+const express = require('express')
 const hljs = require('highlight.js')
-const fs = require('fs');
-const path = require('path');
+const fs = require('fs')
+const path = require('path')
+const markdownItLatex = require('markdown-it-latex').default;
+console.log(markdownItLatex)
 const markdown = require('markdown-it')({
   html: true,
   highlight: function (str, lang) {
     if (lang && hljs.getLanguage(lang)) {
       try {
-        return hljs.highlight(str, { language: lang }).value;
+        return hljs.highlight(str, { language: lang }).value
       } catch (__) {}
     }
 
-    return '';
+    return ''
   }
-});
+}).use(require('markdown-it-latex2img'))
 
 const markdownRender = (content) => {
-  return markdown.render(content)
+  return markdown
+    .render(content)
     .replaceAll(/<h1>(.*)<\/h1>/g, '<h1 id="$1">$1</h1>')
     .replaceAll(/<h2>(.*)<\/h2>/g, '<h2 id="$1">$1</h2>')
     .replaceAll(/<h3>(.*)<\/h3>/g, '<h3 id="$1">$1</h3>');
 }
 
 const move = (source, target, callback) => {
-  let sourceContent = fs.readFileSync(source);
-  if (typeof callback === 'function') sourceContent = callback(sourceContent);
-  fs.writeFileSync(target, sourceContent);
+  let sourceContent = fs.readFileSync(source)
+  if (typeof callback === 'function') sourceContent = callback(sourceContent)
+  fs.writeFileSync(target, sourceContent)
 }
 
 const posts = fs.readdirSync(path.resolve(__dirname, './source/posts'))
-posts.forEach(post => {
+posts.forEach((post) => {
   move(
     path.resolve(__dirname, './source/posts', post),
     path.resolve(__dirname, '../public/posts', post.replace('.md', '.html')),
-    stream => (
+    (stream) =>
       `<html>
         <head>
           <link rel="stylesheet" href="../css/code-theme.css">
           <link rel="stylesheet" href="../css/post.css">
+          <link rel="stylesheet" href="../css/latex.css">
         </head>
         <body>
           <div class="post">
@@ -45,26 +49,28 @@ posts.forEach(post => {
           </div>
         </body>
       </html>`
-    )
   )
 })
 
-fs.cpSync(
-  path.resolve(__dirname, 'source/css'),
-  path.resolve(__dirname, '../public/css'),
-  {
-    recursive: true
-  }
-)
+fs.cpSync(path.resolve(__dirname, 'source/css'), path.resolve(__dirname, '../public/css'), {
+  recursive: true,
+})
 
 move(
   path.resolve(__dirname, '../node_modules/highlight.js/styles/atom-one-dark.css'),
   path.resolve(__dirname, '../public/css', 'code-theme.css')
 )
 
-const app = express();
+move(
+  path.resolve(__dirname, '../node_modules/markdown-it-latex/dist/index.css'),
+  path.resolve(__dirname, '../public/css/latex.css')
+)
 
-app.use('/', express.static(path.resolve(__dirname, '../public')));
+console.log(markdown.render('```late\n$dp[1] = 1;\\\\dp[2] = 2;\\\\dp[3] = 1 + 2 = 3;\\\\dp[4] = 2 + 3 = 5$```'))
+
+const app = express()
+
+app.use('/', express.static(path.resolve(__dirname, '../public')))
 
 app.listen(7777, () => {
   console.log('server started on 7777')
